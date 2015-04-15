2ab58b21e907bcbc7c31245b85975e9f8ddde032
didimitrov <di_dimitrov_@abv.bg>
2015-04-15 12:09:17 +0300

    Created empty Data, Models and Web projects

---
+++ MyOnlineShop/MyOnlineShop.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyOnlineShop.Web.MvcApplication" Language="C#" %>
---
+++ MyOnlineShop/MyOnlineShop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyOnlineShop.Web.Startup))]
+namespace MyOnlineShop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
