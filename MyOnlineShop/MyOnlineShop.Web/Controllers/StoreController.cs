076440265cc269ec4c9b82d9b9880ea97aafb827
didimitrov <di_dimitrov_@abv.bg>
2015-05-18 19:35:06 +0300

    Added Ninject, Replace db with IReposytory in StoreController

--- MyOnlineShop/MyOnlineShop.Web/Controllers/StoreController.cs
+++ MyOnlineShop/MyOnlineShop.Web/Controllers/StoreController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using MyOnlineShop.Data;
+using MyOnlineShop.Data.Repository;
 using MyOnlineShop.Models;
 using MyOnlineShop.Web.ViewModels;
 
@@ -12,12 +13,23 @@ namespace MyOnlineShop.Web.Controllers
 {
     public class StoreController : Controller
     {
-        readonly ApplicationDbContext _db= new ApplicationDbContext();
+        //readonly ApplicationDbContext _db= new ApplicationDbContext();
+        private readonly IRepository<Category> _category;
+        private readonly IRepository<Vote> _vote;
+        private readonly IRepository<Product> _product;
+
+        public StoreController(IRepository<Category> category, IRepository<Vote> vote, IRepository<Product> product)
+        {
+            _category = category;
+            _vote = vote;
+            _product = product;
+        }
+
 
         // GET: Store
         public ActionResult Index()
         {
-            var categories = _db.Categories.ToList();
+            var categories = _category.All().ToList();
 
 
            
@@ -26,7 +38,7 @@ namespace MyOnlineShop.Web.Controllers
 
         public ActionResult Browse(string category)
         {
-            var categoryModel = _db.Categories.Include("Products").SingleOrDefault(x => x.Name==category);
+            var categoryModel = _category.All().Include("Products").SingleOrDefault(x => x.Name==category);
             //var categoryModel = new Category {Name = category};
             return View(categoryModel);
         }
@@ -35,47 +47,53 @@ namespace MyOnlineShop.Web.Controllers
         public ActionResult Details(int id)
         {
             var userId = User.Identity.GetUserId();
-            var product = _db.Products.Find(id);
+            var product = _product.All().SingleOrDefault(x => x.Id==id);
 
-            var productViewModel = new ProductDetailsViewModel
+            if (product != null)
             {
-                Category = product.Category,
-                CategoryId = product.CategoryId,
-                Id = product.Id,
-                Price = product.Price,
-                ProductArtUrl = product.ProductArtUrl,
-                Title = product.Title,
-                //todo change db with repo
-                UserCanVote = _db.Votes.Any(pesho => pesho.VotedById == userId),
-                Votes = product.Votes.Count()
-            };
+                var productViewModel = new ProductDetailsViewModel
+                {
+                    Category = product.Category,
+                    CategoryId = product.CategoryId,
+                    Id = product.Id,
+                    Price = product.Price,
+                    ProductArtUrl = product.ProductArtUrl,
+                    Title = product.Title,
+                    //todo change db with repo
+                    UserCanVote = _vote.All().Any(pesho => pesho.VotedById == userId),
+                    Votes = product.Votes.Count()
+                };
 
-            return View(productViewModel);
+                return View(productViewModel);
+            }
+            return Content("Product is null");
         }
 
         [ChildActionOnly]
         public ActionResult CategoriesMenu()
         {
-            var categories = _db.Categories.ToList();
+            var categories = _category.All().ToList();
             return PartialView(categories);
         }
 
         public ActionResult Vote(int id)
         {
             var userId = User.Identity.GetUserId();
-            var canVote = !_db.Votes.Any(x => x.ProductId == id && x.VotedById == userId);
+            var canVote = !_vote.All().Any(x => x.ProductId == id && x.VotedById == userId);
 
             if (canVote)
             {
-                _db.Products.Find(id).Votes.Add(new Vote()
-                {
-                    ProductId = id,
-                    VotedById = userId
-                });
-                _db.SaveChanges();
+                var singleOrDefault = _product.All().SingleOrDefault(x => x.Id==id);
+                if (singleOrDefault != null)
+                    singleOrDefault.Votes.Add(new Vote
+                    {
+                        ProductId = id,
+                        VotedById = userId
+                    });
+                _product.SaveChanges();
             }
 
-            var votes = _db.Products.Find(id).Votes.Count();
+            var votes = _product.GetById(id).Votes.Count();
 
             if (votes != 0)
             {
